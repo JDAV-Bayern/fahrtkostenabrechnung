f99a56333058ee62071ced6229f6f9e7818c3627
joseph <joseph@maxau.de>
2023-10-15 18:09:02 +0200

    buttons submisstion page

--- src/app/submission-overview-component/submission-overview-component.component.ts
+++ src/app/submission-overview-component/submission-overview-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-submission-overview-component',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class SubmissionOverviewComponentComponent {
 
+  constructor(private readonly router: Router) { }
+  continue() {
+
+  }
+  back() {
+    this.router.navigate(['auslagen']);
+  }
 }
