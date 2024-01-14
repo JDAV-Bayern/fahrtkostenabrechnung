9193a458e38da6fff73179c2e27169a00b8d98e0
joseph <joseph@maxau.de>
2024-01-14 19:47:56 +0100

    basic pdf stuff, highly wip

--- src/app/expense-list-row/expense-list-row.component.ts
+++ src/app/expense-list-row/expense-list-row.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { IExpense } from 'src/domain/expense';
+import { IBikeExpense, ICarExpense, IExpense, IPublicTransportPlanExpense, ITrainExpense } from 'src/domain/expense';
 
 @Component({
   selector: 'app-expense-list-row',
@@ -43,6 +43,31 @@ export class ExpenseListRowComponent {
         title = '';
     }
     return title;
+  }
+
+  getDetails() {
+    let details: string;
+    switch (this.expense.type) {
+      case 'car':
+        const carExpense = this.expense as ICarExpense;
+        details = `${carExpense.distance} km, ${carExpense.passengers.length} Mitfahrer*innen`
+        break;
+      case 'train':
+        const trainExpense = this.expense as ITrainExpense;
+        details = `${trainExpense.discountCard === 'BC50' ? 'BahnCard 50' : trainExpense.discountCard === 'BC25' ? 'BahnCard 25' : 'keine BahnCard'}`;
+        break;
+      case 'plan':
+        const planExpense = this.expense as IPublicTransportPlanExpense;
+        details = `Abo-Preis: ${planExpense.price} €`;
+        break;
+      case 'bike':
+        const bikeExpense = this.expense as IBikeExpense;
+        details = `${bikeExpense.distance} km`;
+        break;
+      default:
+        details = '';
+    }
+    return details;
   }
 
 
--- src/app/submission-overview-component/submission-overview-component.component.ts
+++ src/app/submission-overview-component/submission-overview-component.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import jsPDF from 'jspdf';
+
+import { validateIBAN } from "ngx-iban-validator";
+import { IReimbursement } from 'src/domain/reimbursement';
+import { ReimbursementService } from '../reimbursement.service';
+
 
 @Component({
   selector: 'app-submission-overview-component',
@@ -8,11 +15,55 @@ import { Router } from '@angular/router';
 })
 export class SubmissionOverviewComponentComponent {
 
-  constructor(private readonly router: Router) { }
-  continue() {
+  formGroup: FormGroup
+  fileFormGroup: FormGroup;
 
+  reimbursement: IReimbursement | undefined
+
+  constructor(private readonly router: Router, private formBuilder: FormBuilder, private readonly reimbursementService: ReimbursementService) {
+    this.formGroup = this.formBuilder.group({
+      inputIBAN: ['', [Validators.required, validateIBAN]],
+      inputBIC: ['', []],
+    })
+    this.fileFormGroup = this.formBuilder.group({
+      file: [undefined, []]
+    });
+  }
+
+  async continue() {
+    console.log("generating pdf...");
+    const htmlElement = document.getElementById("pdf-container");
+    if (!htmlElement) {
+      return;
+    }
+    const doc = new jsPDF('p', 'pt', [595, 822], true);
+    await new Promise<void>(resolve => doc.html(htmlElement, {
+      autoPaging: "text"
+    }).finally(() => resolve()));
+    doc.save("auslagen.pdf");
   }
   back() {
     this.router.navigate(['auslagen']);
   }
+  submitForm() {
+  }
+  selectFile() {
+    console.log(this.fileFormGroup);
+  }
+  getDate() {
+    //Get date in the format DD.MM.YYYY
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+
+  r(): IReimbursement {
+    if (!this.reimbursement) {
+      this.reimbursement = this.reimbursementService.getReimbursment();
+    }
+    return this.reimbursement;
+  }
+
 }
