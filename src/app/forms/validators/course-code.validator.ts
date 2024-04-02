516d46ee5782e39e9ace82934f14964a0a1d6161
mleser <164559062+mleser@users.noreply.github.com>
2024-04-02 18:33:44 +0200

    Update course-code.validator.ts
    Hey 
    Ich hab das erste mal mit diesem Programm zu tun und hab keine Ahnung ob das stimmt. Da ich aber sowieso immer mal mehr Programmieren wollte hab ich es jetzt einfach mal ausprobiert ich hoffe ich hab nichts kaputt gemacht. 
    Meine Idee war einen eigenen Kurs_Code für die Landesjugendleitung (LJL) zu implementieren.

--- src/app/forms/validators/course-code.validator.ts
+++ src/app/forms/validators/course-code.validator.ts
@@ -2,12 +2,16 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 const COURSE_CODE_BY = /^B[0-9]{3}(FB|AM|GA)$/;
 const COURSE_CODE_DE = /^J[0-9]{3}$/;
+const COURSE_CODE_LJL = /^LJL$/
 
 export function validateCourseCode(
   control: AbstractControl
 ): ValidationErrors | null {
   const v = control.value;
   if (v === null || v.length === 0 || v.match(COURSE_CODE_BY)) {
+    return null;
+  }
+  if (v.match(COURSE_CODE_LJL)) {
     return null;
   }
   if (v.match(COURSE_CODE_DE)) {
