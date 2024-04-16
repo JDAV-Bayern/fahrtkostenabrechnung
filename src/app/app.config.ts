9c2bd289bec183a8acf2692fa6093aee41cdb280
Simon Langebrake <s.langebrake@gmail.com>
2024-04-16 10:39:50 +0200

    Add datepicker dependency Also fix ng-select version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,17 @@ import { APP_BASE_HREF } from '@angular/common';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
+import {
+  OWL_DATE_TIME_LOCALE,
+  OwlDateTimeIntl
+} from '@danielmoncada/angular-datetime-picker';
+import { DefaultIntl } from './datetime-intl';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withHashLocation()),
-    { provide: APP_BASE_HREF, useValue: '/fahrtkostenabrechnung/' }
+    { provide: APP_BASE_HREF, useValue: '/fahrtkostenabrechnung/' },
+    { provide: OWL_DATE_TIME_LOCALE, useValue: 'de' },
+    { provide: OwlDateTimeIntl, useClass: DefaultIntl }
   ]
 };
