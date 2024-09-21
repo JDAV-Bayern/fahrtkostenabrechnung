8f692ca51491ce6f81ba37850e7399088a87ade3
joseph <joseph@maxau.de>
2024-09-21 20:54:59 +0200

    show version in data protection view

--- src/app/info/data-protection/data-protection.component.ts
+++ src/app/info/data-protection/data-protection.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { version } from '../../../../package.json';
 @Component({
   selector: 'app-data-protection',
   templateUrl: './data-protection.component.html',
   styleUrls: ['./data-protection.component.css'],
   standalone: true
 })
-export class DataProtectionComponent {}
+export class DataProtectionComponent {
+  version = version;
+}
