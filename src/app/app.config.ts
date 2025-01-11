7c3bf6cd9ac56fed05469b6ebe04473158481627
Simon Langebrake <s.langebrake@gmail.com>
2025-01-11 12:01:55 +0100

    Update Material theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,7 @@
 import { DEFAULT_DIALOG_CONFIG, DialogConfig } from '@angular/cdk/dialog';
-import {
-  ApplicationConfig,
-  DEFAULT_CURRENCY_CODE,
-  importProvidersFrom
-} from '@angular/core';
-import { DateFnsModule } from '@angular/material-date-fns-adapter';
-import {
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-  MatDateFormats
-} from '@angular/material/core';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE } from '@angular/core';
+import { provideDateFnsAdapter } from '@angular/material-date-fns-adapter';
+import { MAT_DATE_LOCALE, MatDateFormats } from '@angular/material/core';
 import { MatDatepickerIntl } from '@angular/material/datepicker';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withHashLocation } from '@angular/router';
@@ -45,11 +37,10 @@ export const appConfig: ApplicationConfig = {
       ...(environment.useHashRouting ? [withHashLocation()] : [])
     ),
     provideAnimations(),
-    importProvidersFrom(DateFnsModule),
+    provideDateFnsAdapter(DATE_FORMATS),
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
     { provide: DEFAULT_DIALOG_CONFIG, useValue: DIALOG_CONFIG },
     { provide: MAT_DATE_LOCALE, useValue: de },
-    { provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS },
     { provide: MatDatepickerIntl, useClass: JdavDatepickerIntl }
   ]
 };
