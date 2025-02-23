92c75dee4639f5d3f727036b3229735dd94bddd3
MeiNic <137501539+MeiNic@users.noreply.github.com>
2025-02-23 12:47:34 +0100

    Update src/app/core/header/header.component.ts
    Co-authored-by: lesti <51439715+lesti@users.noreply.github.com>

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -20,20 +20,10 @@ export class HeaderComponent implements OnInit {
   }
 
   deleteAllData() {
-    let originUrl: string;
-    switch (this.controlService.meetingStep.value.type) {
-      case 'course':
-        originUrl = 'kurs';
-        break;
-      case 'assembly':
-        originUrl = 'ljv';
-        break;
-      case 'committee':
-        originUrl = 'gremium';
-        break;
-      default:
-        originUrl = 'kurs';
-    }
+    const originUrl: string = {
+      'assembly': 'ljv',
+      'committee': 'gremium',
+      }[this.controlService.meetingStep.value.type] ?? 'kurs';
     this.controlService.deleteStoredData();
     this.router.navigate([originUrl]);  }
 }
