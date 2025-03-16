1ace14062bf103e1e60149076b68fc8d383c8d94
joseph <joseph@maxau.de>
2025-03-16 23:14:03 +0100

    run formater

--- src/app/admin/admin-courses/admin-courses.component.ts
+++ src/app/admin/admin-courses/admin-courses.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './admin-courses.component.html',
   styleUrl: './admin-courses.component.css'
 })
-export class AdminCoursesComponent {
-
-}
+export class AdminCoursesComponent {}
