e3e6ba3de6e9ef2b077659a2eae2eac7fc4c75d4
Simon Langebrake <s.langebrake@gmail.com>
2025-09-06 23:09:21 +0200

    Fix docker build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
-export interface Environment {
-  environment: 'development' | 'staging' | 'production';
-}
+import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   environment: 'production'
