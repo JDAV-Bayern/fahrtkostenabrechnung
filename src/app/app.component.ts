4cc0dafe8b10d16fe2c809e178c015ad2a3e4b09
joseph <joseph@maxau.de>
2026-02-15 23:40:07 +0100

    feat: add staging banner to inform users about the staging environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { FooterComponent } from './core/footer/footer.component';
 import { HeaderComponent } from './core/header/header.component';
 
@@ -12,4 +13,6 @@ import { HeaderComponent } from './core/header/header.component';
       'grid grid-rows-[auto_1fr_auto] min-h-screen font-titillium text-gray-950 antialiased',
   },
 })
-export class AppComponent {}
+export class AppComponent {
+  showStagingBanner = environment.environment === 'staging';
+}
