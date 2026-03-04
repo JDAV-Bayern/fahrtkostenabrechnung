709e5ec1eb76329ded015a9d1f84cc4c74138719
joseph <joseph@maxau.de>
2026-03-04 22:37:32 +0100

    feat: update expense details to use dynamic plan reimbursement amount

--- src/app/reimbursement/expenses/shared/expense-details/expense-details.component.ts
+++ src/app/reimbursement/expenses/shared/expense-details/expense-details.component.ts
@@ -1,7 +1,8 @@
 import { CurrencyPipe, DecimalPipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { JoinPipe } from 'src/app/shared/join.pipe';
 import { Expense } from 'src/domain/expense.model';
+import { ExpenseConfigService } from '../expense-config.service';
 import { DiscountPipe, EngineTypePipe, MealsPipe } from '../expense-data.pipe';
 
 @Component({
@@ -16,6 +17,18 @@ import { DiscountPipe, EngineTypePipe, MealsPipe } from '../expense-data.pipe';
     MealsPipe,
   ],
 })
-export class ExpenseDetailsComponent {
+export class ExpenseDetailsComponent implements OnInit {
   readonly expense = input.required<Expense>();
+
+  readonly expenseConfigService = inject(ExpenseConfigService);
+
+  planFixedReimbursementAmount: number | null = null;
+
+  ngOnInit() {
+    if (this.expense().type === 'transport') {
+      this.expenseConfigService.getConfig('course').subscribe((config) => {
+        this.planFixedReimbursementAmount = config.transport?.plan ?? null;
+      });
+    }
+  }
 }
